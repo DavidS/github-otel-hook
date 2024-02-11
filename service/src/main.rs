487860e90559de7eb0bad7a7730892ee175a85ea
David Schmitt <david@black.co.at>
2024-02-11 14:47:04 +0100

    Add first sketch for webhook receiver

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,4 +1,8 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::{HeaderMap, StatusCode},
+    routing::{get, post},
+    Router,
+};
 use tracing::instrument;
 use tracing_subscriber::fmt::format::FmtSpan;
 
@@ -6,6 +10,18 @@ use tracing_subscriber::fmt::format::FmtSpan;
 async fn index() -> String {
     tracing::info!("inside index!");
     String::from("homepage")
+}
+
+#[instrument(level = "info")]
+async fn webhook(headers: HeaderMap) -> Result<String, StatusCode> {
+    if let Some(evt) = headers.get("X-GitHub-Event") {
+        return evt
+            .to_str()
+            .map(|s| s.to_string())
+            .map_err(|_| StatusCode::BAD_REQUEST);
+    }
+
+    Err(StatusCode::BAD_REQUEST)
 }
 
 #[tokio::main]
@@ -18,7 +34,8 @@ async fn main() {
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(index));
+        .route("/", get(index))
+        .route("/webhook", post(webhook));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
