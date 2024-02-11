22ab78896d67409ab38c99aad4c31c6499343652
David Schmitt <david@black.co.at>
2024-02-11 10:32:31 +0100

    First runnable axum service

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+async fn index() -> String {
+    String::from("homepage")
+}
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(index));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
